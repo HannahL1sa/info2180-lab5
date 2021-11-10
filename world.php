4707cce1d904793d6028a51d473da7fe0ec7c658
Hannah-Lisa Whittle <whittlehannahlisa@gmail.com>
2021-11-09 21:05:12 -0500

    Display Cities in a Country

--- world.php
+++ world.php
@@ -4,35 +4,59 @@ $username = 'lab5_user';
 $password = 'password123';
 $dbname = 'world';
 
-$query= "";
-// Read $_GET value
+
 if(isset($_GET['country'])){
-  $query = $_GET['country'];
+  $country = filter_input(INPUT_GET,"country",FILTER_SANITIZE_STRING);
 }
-//$query = $_GET['country'];
+
+$context = '';
+if(isset($_GET['context'])){
+  $context = $_GET['context'];
+}
+
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-//$stmt = $conn->query("SELECT * FROM countries");
-$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$query%'");
+//First query
+$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+$results= $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
+//Second Query
+$statement = $conn->query("SELECT cities.name, cities.population, cities.district  FROM cities JOIN countries ON cities.country_code=countries.code WHERE countries.name LIKE '%$country%'");
+$cities = $statement->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
-
-<table>
+<?php if($context === 'cities'):?>
+  <table>
+    <tr>
+      <th>Name</th>
+      <th>District</th>
+      <th>Population</th>
+</tr>
+<?php foreach ($cities as $city):?>
   <tr>
-    <th scope="col">Name</th>
-    <th scope="col">Continent</th>
-    <th scope="col">Independence</th>
-    <th scope="col">Head of State</th>
-  </tr>
+    <td><?=$city['name'];?></td>
+    <td><?=$city['district'];?></td>
+    <td><?=$city['population'];?></td>
+</tr>
+<?php endforeach;?>
+</table>
+<?php else:?>
+  <table>
+    <tr>
+      <th>Name</th>
+      <th>Continent</th>
+      <th>Independence</th>
+      <th>Head of State</th>
+    </tr>
     <?php foreach ($results as $row): ?>
       <tr>
         <td><?= $row['name'];?></td> 
         <td><?= $row['continent'];?></td> 
         <td><?= $row['independence_year'];?></td>
         <td><?= $row['head_of_state'];?></td> 
-    </tr>
-    <?php endforeach; ?>
+      </tr>
+      <?php endforeach; ?>
 </table>
+<?php endif;?>
+
+
 
